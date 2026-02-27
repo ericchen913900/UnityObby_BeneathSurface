a63c894689d84ab462221897a0eae9002c6684a3
Eric Chen <ericchen913900@users.noreply.github.com>
2026-02-27 14:15:09 +0800

    feat(player): migrate movement and camera to 2D

--- Assets/Scripts/Player/FollowCamera.cs
+++ Assets/Scripts/Player/FollowCamera.cs
@@ -5,10 +5,28 @@ namespace BeneathSurface.Player
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] private Transform target;
-        [SerializeField] private Vector3 offset = new Vector3(0f, 4f, -6f);
+        [SerializeField] private Vector2 offset = new Vector2(3f, 2.4f);
+        [SerializeField] private float fixedZ = -10f;
+        [SerializeField] private bool forceOrthographic = true;
+        [SerializeField] private float orthographicSize = 5.6f;
         [SerializeField] private float smoothTime = 0.15f;
 
         private Vector3 _velocity;
+
+        private void Awake()
+        {
+            if (!forceOrthographic)
+            {
+                return;
+            }
+
+            var cam = GetComponent<Camera>();
+            if (cam != null)
+            {
+                cam.orthographic = true;
+                cam.orthographicSize = orthographicSize;
+            }
+        }
 
         private void LateUpdate()
         {
@@ -17,9 +35,8 @@ namespace BeneathSurface.Player
                 return;
             }
 
-            var desired = target.position + target.TransformDirection(offset);
+            var desired = new Vector3(target.position.x + offset.x, target.position.y + offset.y, fixedZ);
             transform.position = Vector3.SmoothDamp(transform.position, desired, ref _velocity, smoothTime);
-            transform.LookAt(target.position + Vector3.up * 1.2f);
         }
 
         public void Configure(Transform followTarget)
--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -5,47 +5,69 @@ using UnityEngine.InputSystem;
 
 namespace BeneathSurface.Player
 {
-    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class PlayerMotor : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 7f;
         [SerializeField] private float jumpHeight = 1.8f;
         [SerializeField] private float gravity = -20f;
+        [SerializeField] private float groundProbeDistance = 0.08f;
+        [SerializeField] private LayerMask groundMask = ~0;
 
-        private CharacterController _controller;
-        private Vector3 _velocity;
+        private Rigidbody2D _body;
+        private Collider2D _collider;
+        private float _moveInput;
+        private bool _jumpQueued;
 
         private void Awake()
         {
-            _controller = GetComponent<CharacterController>();
+            _body = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<Collider2D>();
+
+            _body.freezeRotation = true;
+            _body.gravityScale = Mathf.Abs(gravity) / 9.81f;
         }
 
         private void Update()
         {
             var input = ReadMoveInput();
-            var direction = transform.right * input.x + transform.forward * input.y;
-            _controller.Move(direction.normalized * (moveSpeed * Time.deltaTime));
+            _moveInput = Mathf.Clamp(input.x, -1f, 1f);
 
-            if (_controller.isGrounded && _velocity.y < 0f)
+            if (ReadJumpPressed())
             {
-                _velocity.y = -2f;
+                _jumpQueued = true;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            var velocity = _body.velocity;
+            velocity.x = _moveInput * moveSpeed;
+
+            if (_jumpQueued && IsGrounded())
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             }
 
-            if (_controller.isGrounded && ReadJumpPressed())
-            {
-                _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-
-            _velocity.y += gravity * Time.deltaTime;
-            _controller.Move(_velocity * Time.deltaTime);
+            _body.velocity = velocity;
+            _jumpQueued = false;
         }
 
         public void SnapTo(Vector3 worldPosition)
         {
-            _controller.enabled = false;
-            transform.position = worldPosition;
-            _velocity = Vector3.zero;
-            _controller.enabled = true;
+            var target = new Vector2(worldPosition.x, worldPosition.y);
+            _body.velocity = Vector2.zero;
+            _body.position = target;
+            transform.position = new Vector3(target.x, target.y, transform.position.z);
+        }
+
+        private bool IsGrounded()
+        {
+            var bounds = _collider.bounds;
+            var probeCenter = new Vector2(bounds.center.x, bounds.min.y - groundProbeDistance * 0.5f);
+            var probeSize = new Vector2(bounds.size.x * 0.85f, groundProbeDistance);
+            var hit = Physics2D.OverlapBox(probeCenter, probeSize, 0f, groundMask);
+            return hit != null && hit != _collider;
         }
 
         private static Vector2 ReadMoveInput()
@@ -54,8 +76,10 @@ namespace BeneathSurface.Player
             var keyboard = Keyboard.current;
             if (keyboard != null)
             {
-                var x = (keyboard.dKey.isPressed ? 1f : 0f) - (keyboard.aKey.isPressed ? 1f : 0f);
-                var y = (keyboard.wKey.isPressed ? 1f : 0f) - (keyboard.sKey.isPressed ? 1f : 0f);
+                var x = (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed ? 1f : 0f)
+                    - (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed ? 1f : 0f);
+                var y = (keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed ? 1f : 0f)
+                    - (keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed ? 1f : 0f);
                 return new Vector2(x, y);
             }
 #endif
@@ -68,7 +92,7 @@ namespace BeneathSurface.Player
             var keyboard = Keyboard.current;
             if (keyboard != null)
             {
-                return keyboard.spaceKey.wasPressedThisFrame;
+                return keyboard.spaceKey.wasPressedThisFrame || keyboard.upArrowKey.wasPressedThisFrame;
             }
 #endif
             return Input.GetButtonDown("Jump");
